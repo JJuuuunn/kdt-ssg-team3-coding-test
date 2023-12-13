772766b5d5f447c5c426fd3bd83a8ce8590aefeb
yunyeobin <sky_line22@naver.com>
2023-12-13 20:50:08 +0900

    init

--- src/윤여빈/백준2884.java
+++ src/윤여빈/백준2884.java
@@ -1,7 +1,18 @@
 package 윤여빈;
 
+import java.util.Scanner;
+
 public class 백준2884 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if (a == 0 && b == 0){
+                break;
+            }
+            System.out.println(a+b);
+        }
 
     }
 }
