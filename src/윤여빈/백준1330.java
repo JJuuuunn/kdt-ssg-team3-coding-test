baf292b16f4aedf71a54f9c9aa7f97d8c322c0b4
yunyeobin <sky_line22@naver.com>
2023-12-13 20:50:53 +0900

    init

--- src/윤여빈/백준1330.java
+++ src/윤여빈/백준1330.java
@@ -1,7 +1,16 @@
 package 윤여빈;
 
+import java.util.Scanner;
+
 public class 백준1330 {
     public static void main(String[] args) {
-        System.out.println("Test   ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = 0;
+        for (int i = 1; i <= n; i++) {
+            a += i;
+
+        }
+        System.out.println(a);
     }
 }
