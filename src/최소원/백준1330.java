c1b2e863940101a12fe702880927769218ce62fc
sowon901 <87898185+sowon901@users.noreply.github.com>
2023-12-14 23:10:36 +0900

    백준 조건문, 반복문, 배열 제출

--- src/최소원/백준1330.java
+++ src/최소원/백준1330.java
@@ -1,7 +1,17 @@
 package 최소원;
+import java.util.*;
 
 public class 백준1330 {
     public static void main(String[] args) {
-        System.out.println("test");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a>b)
+            System.out.println(">");
+        else if(a<b)
+            System.out.println("<");
+        else
+            System.out.println("==");
     }
 }
--- src/최소원/백준8393.java
+++ src/최소원/백준8393.java
@@ -1,7 +1,16 @@
 package 최소원;
+import java.util.*;
 
 public class 백준8393 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int n= sc.nextInt();
+        int total = 0;
+
+        for(int i=1; i<=n; i++) {
+            total +=i;
+        }
+        System.out.println(total);
     }
 }
