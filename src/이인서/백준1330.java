20e5a02858008f5668e3b7c8c8204bec565abbce
Is-5pm <ganet9811@gmail.com>
2023-12-14 09:43:55 +0900

    12.15 코드 리뷰용 백준 풀이

--- src/이인서/백준1330.java
+++ src/이인서/백준1330.java
@@ -1,7 +1,20 @@
 package 이인서;
 
+import java.util.Scanner;
+
 public class 백준1330 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //A B 입력 받음
+        int A = sc.nextInt();
+        int B = sc.nextInt();
 
+        if (A>B) {
+            System.out.println(">");
+        }  else if (A<B) {
+            System.out.println("<");
+        } else if (A==B) {
+            System.out.println("==");
+        }
     }
 }
