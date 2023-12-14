87f1f684a717435ed008fee859c41eb8b3b44453
jjuuuunn <didtdjwns111@gmail.com>
2023-12-14 09:36:41 +0900

    231214(목) 양성준 제출완료
    조건문 문제 1 : https://www.acmicpc.net/problem/2884
    조건문 문제 2 : https://www.acmicpc.net/problem/1330
    반복문 문제 1 : https://www.acmicpc.net/problem/10952
    반복문 문제 2 : https://www.acmicpc.net/problem/8393
    배열 문제 1 : https://www.acmicpc.net/problem/10807
    배열 문제 2 :https://www.acmicpc.net/problem/10818

--- src/양성준/백준2884.java
+++ src/양성준/백준2884.java
@@ -1,7 +1,20 @@
 package 양성준;
 
-public class 백준2884 {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.*;
 
+public class 백준2884 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int h = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        if (m - 45 < 0) { // 45를 뺀 값이 음수인 경우
+            m = Math.abs(60 - (45 - m));
+            if (h - 1 < 0) h = 23; // h가 0인경우 전날로 돌아가야해서 조건 확인
+            else h--;
+        } else m -= 45;
+        System.out.printf("%d %d", h, m);
     }
 }
