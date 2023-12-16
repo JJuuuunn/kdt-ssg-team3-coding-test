b7cf89b2a9d0a73183a8d4aa685f501a7e25d4c5
khr0923 <127317034+khr0923@users.noreply.github.com>
2023-12-16 11:01:42 +0900

    study01_백준8393 제출

--- src/김해린/백준8393.java
+++ src/김해린/백준8393.java
@@ -1,7 +1,17 @@
 package 김해린;
 
-public class 백준8393 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class 백준8393 {
+    static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n = in.nextInt(); //정수 n을 입력 받음
+        int total = 0;
+
+        for(int i = 1; i <= n; i++) { //1부터 입력 받은 n까지 반복
+            total += i; // <- total = total + i (1~n까지의 합을 변수 total에 넣음)
+        }
+
+        System.out.println(total);
     }
 }
