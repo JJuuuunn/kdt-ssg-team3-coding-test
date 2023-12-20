1ba285fc052f32de4394f0e78c8ee1ac16a70979
Konerd <adr10won@gmail.com>
2023-12-20 23:23:03 +0900

    Add solutions  - 2941th, 3003th, 10988th, 25206th problem of baekjoon

--- src/SSG2주차/백준3003/조정우/Main.java
+++ src/SSG2주차/백준3003/조정우/Main.java
@@ -1,4 +1,22 @@
 package SSG2주차.백준3003.조정우;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        final int[] FORM_CHESS = {1, 1, 2, 2, 2, 8};
+        final int LEN_FORM_CHESS = FORM_CHESS.length;
+        Scanner sc = new Scanner(System.in);
+        int[] formChessCurrent = new int[LEN_FORM_CHESS];
+        for (int idxSequenceChess = 0; idxSequenceChess < LEN_FORM_CHESS; idxSequenceChess++) {
+            formChessCurrent[idxSequenceChess] = sc.nextInt();
+        }
+        for (int idxSequenceChess = 0; idxSequenceChess < LEN_FORM_CHESS; idxSequenceChess++) {
+            System.out.printf("%d", FORM_CHESS[idxSequenceChess] - formChessCurrent[idxSequenceChess]);
+            if (idxSequenceChess < LEN_FORM_CHESS - 1) {
+                System.out.print(" ");
+            }
+        }
+        sc.close();
+    }
 }
