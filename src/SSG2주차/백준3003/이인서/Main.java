4a590ab62da9b90c244cc7214ce2a537ae21f348
Is-5pm <ganet9811@gmail.com>
2023-12-21 10:59:14 +0900

    3003
    체스 말 개수 출력

--- src/SSG2주차/백준3003/이인서/Main.java
+++ src/SSG2주차/백준3003/이인서/Main.java
@@ -1,4 +1,16 @@
 package SSG2주차.백준3003.이인서;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] blackChess = new int[]{1,1,2,2,2,8}; //기준 체스 말 개수
+        int[] whiteChess = new int[6]; //흰색 말 개수 입력받을 배열 선언
+
+        for (int i=0; i< whiteChess.length; i++){
+            whiteChess[i] = sc.nextInt(); //배열 채우기
+            System.out.print((blackChess[i]-whiteChess[i])+" "); //개수 차이 출력
+        }
+    }
 }
