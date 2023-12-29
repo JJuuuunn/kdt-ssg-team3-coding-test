5180eb3b8228ca535342b0f359c4edac9d606949
yunyeobin <sky_line22@naver.com>
2023-12-29 17:41:37 +0900

    init

--- src/SSG4주차/가상_대선_당선_시뮬레이션_프로그램/윤여빈/Main.java
+++ src/SSG4주차/가상_대선_당선_시뮬레이션_프로그램/윤여빈/Main.java
@@ -1,6 +1,7 @@
 package SSG4주차.가상_대선_당선_시뮬레이션_프로그램.윤여빈;
 
 import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         double totalVotes = 10000;
@@ -11,18 +12,16 @@ public class Main {
             candidates[(random.nextInt(4) + 0)] += 1;
         }
         int max = 0;
+        int maxman = 0;
         for (int i = 0; i < 4; i++) {
             System.out.printf("[기호:%d] %s: %.2f%%, (투표수: %d)\n", i + 1, candiName[i], candidates[i] / totalVotes * 100.0, candidates[i]);
             if (candidates[i] > max) {
                 max = candidates[i];
+                maxman = i;
             }
         }
-        for (int i = 0; i < 4; i++) {
-            if (max == candidates[i]) {
-                System.out.println("[투표결과] 당선인: " + candiName[i]);
 
-            }
+        System.out.println("[투표결과] 당선인: " + candiName[maxman]);
 
-        }
     }
 }
