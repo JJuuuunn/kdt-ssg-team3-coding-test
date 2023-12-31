1f5f0c0ffdb3a0ce5d5b26bbc30bfea6e3209663
yunyeobin <sky_line22@naver.com>
2023-12-31 14:38:05 +0900

    init

--- src/SSG4주차/백준1085/윤여빈/Main.java
+++ src/SSG4주차/백준1085/윤여빈/Main.java
@@ -1,4 +1,37 @@
 package SSG4주차.백준1085.윤여빈;
 
+import java.util.Scanner;
+
 public class Main {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int w = sc.nextInt();
+        int h = sc.nextInt();
+        if (w-x>x && h-y>y){
+            if(x>y){
+                System.out.println(y);
+            }else {
+                System.out.println(x);
+            }
+        }
+        else if (w-x<x && h-y<y){
+            if(w-x>h-y){
+                System.out.println(h-y);
+            }else{
+                System.out.println(w-x);
+            }
+        }
+        else if(w-x>x && h-y<y){
+            System.out.println(h-y);
+        }else if (w-x<x && h-y>y){
+            if (w-x>y){
+                System.out.println(y);
+            }else{
+                System.out.println(w-x);
+            }
+        }
+
+    }
 }
