274abdb6528cc65a96026852ee7def62095e9cbe
JiniiW <ksjine2@gmail.com>
2024-01-04 01:14:15 +0900

    3주차 리펙터링
    3주차 리펙터링

--- src/SSG4주차/가상_대선_당선_시뮬레이션_프로그램/김소진/Main.java
+++ src/SSG4주차/가상_대선_당선_시뮬레이션_프로그램/김소진/Main.java
@@ -1,30 +1,49 @@
 package SSG4주차.가상_대선_당선_시뮬레이션_프로그램.김소진;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         String[] candidate = {"이재명", "윤석열", "심상정", "안철수"};
         int[] votes = new int[4];
-        //10000번 반복
-        for (int i = 1; i <= 10000; i++) {
-            //random함수 투표
-            int vote = (int)(Math.random()*4+1);
-            if(vote == 1){
-                votes[0] += 1;
-            } else if (vote == 2) {
-                votes[1] += 1;
-            } else if (vote == 3) {
-                votes[2] += 1;
-            } else if (vote == 4) {
-                votes[3] += 1;
+
+        final int MAX_VOTES = 10000; //상수 선언
+        //표 입력받기
+        int j = 1;
+        while (j <= MAX_VOTES) {
+            int vote = (int) (Math.random() * 4 + 1);
+            switch (vote) {
+                case 1 -> votes[0] += 1;
+                case 2 -> votes[1] += 1;
+                case 3 -> votes[2] += 1;
+                case 4 -> votes[3] += 1;
+
             }
+            j++;
         }
+
+//        for (int i = 1; i <= 10000; i++) {
+//            //random함수 투표
+//            int vote = (int)(Math.random()*4+1);
+//            if(vote == 1){
+//                votes[0] += 1;
+//            } else if (vote == 2) {
+//                votes[1] += 1;
+//            } else if (vote == 3) {
+//                votes[2] += 1;
+//            } else if (vote == 4) {
+//                votes[3] += 1;
+//            }
+//        }}
 
         int max = 0;
         int index = 0;
+
         //출력for문
-        for(int i = 0; i < candidate.length; i++){
+        for (int i = 0; i < candidate.length; i++) {
             //투표율
-            double ratio = (double) votes[i] / 100;
+            double ratio = (double) votes[i] / (MAX_VOTES/100); //비율 유지
             System.out.printf("[기호 : %d] %s: %.2f%%, (투표수: %d)", (i + 1), candidate[i], ratio, votes[i]);
             System.out.println();
             //당선인
@@ -32,7 +51,11 @@ public class Main {
                 max = votes[i];
                 index = i;
             }
+
+            //Stream은...
+            //max = Arrays.stream(votes).max().getAsInt();
         }
+
         System.out.printf("[투표결과] 당선인: %s", candidate[index]);
     }
 }
--- src/SSG4주차/로또_당첨_프로그램/김소진/Main.java
+++ src/SSG4주차/로또_당첨_프로그램/김소진/Main.java
@@ -1,6 +1,7 @@
 package SSG4주차.로또_당첨_프로그램.김소진;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
@@ -10,64 +11,74 @@ public class Main {
         System.out.println("[로또 당첨 프로그램]\n");
         System.out.print("로또 개수룰 입력해주세요.(숫자 1 ~ 10) : ");
         int count = sc.nextInt(); //로또 개수 입력
-        int[][] mylotto = new int[count][6]; //나의 로또 배열 선언
-        for (int i = 0; i < mylotto.length; i++) {
+//        int[][] mylotto = new int[count][6]; //나의 로또 배열 선언
+        ArrayList<Integer>[] mylotto = new ArrayList[count];
+        for (int i = 0; i < count; i++) {
             System.out.print((char) (i + 65) + "\t");
-            lottoNumber(mylotto[i]); //로또 번호 생성 및 출력 메서드 호출
+            mylotto[i] = lottoNumber();
             System.out.println();
         }
 
         //로또 발표
         System.out.println("\n[로또 발표]");
-        int[] lotto = new int[6]; //당첨 로또 배열 선언
+        ArrayList<Integer> lotto;
         System.out.print("\t");
-        lottoNumber(lotto); //로또 번호 생성 및 출력 메서드 호출
+        lotto = lottoNumber();
         System.out.println("\n");
 
 
         //로또 번호 비교
         System.out.println("[내 로또 결과]");
-        for (int i = 0; i < mylotto.length; i++) { //입력받은 개수만큼 for문 진행
+        for (int i = 0; i < count; i++) {
             System.out.print((char) (i + 65) + "\t");
-            int duplicationNumber = 0; //중복값 초기화
-            for (int j = 0; j < mylotto[i].length; j++) { //해당 행의 로또 번호 비교
-                System.out.printf("%2d", mylotto[i][j]);
-                for (int k = 0; k < 6; k++) { //기존 lotto의 값과 mylotto의 값을 비교
-                    if (lotto[k] == mylotto[i][j]) {
-                        duplicationNumber++;
-                    }
-                }
-                if(j < 5){
-                    System.out.print(", ");
-                }
-            }
-            System.out.printf(" => %d개 일치", duplicationNumber);
+            printLottoNumbers(mylotto[i]);
+            mylotto[i].retainAll(lotto);
+            System.out.print("-> " + mylotto[i].size() + "개");
             System.out.println();
         }
+//        for (int i = 0; i < mylotto.length; i++) { //입력받은 개수만큼 for문 진행
+//            System.out.print((char) (i + 65) + "\t");
+//            int duplicationNumber = 0; //중복값 초기화
+//            for (int j = 0; j < mylotto[i].length; j++) { //해당 행의 로또 번호 비교
+//                System.out.printf("%2d", mylotto[i][j]);
+//                for (int k = 0; k < 6; k++) { //기존 lotto의 값과 mylotto의 값을 비교
+//                    if (lotto[k] == mylotto[i][j]) {
+//                        duplicationNumber++;
+//                    }
+//                }
+//                if(j < 5){
+//                    System.out.print(", ");
+//                }
+//            }
+//            System.out.printf(" => %d개 일치", duplicationNumber);
+//            System.out.println();
+//        }
+    }
+
+    //로또번호 추가 메서드
+    static ArrayList<Integer> lottoNumber() {
+        ArrayList<Integer> lottoNumber = new ArrayList<>();
+        //ArrayList에 1부터 45까지 넣어줌
+        for (int i = 1; i <= 45; i++) {
+            lottoNumber.add(i);
+        }
+        //list의 순서 변경
+        Collections.shuffle(lottoNumber);
+        //6자리를 출력
+        lottoNumber = new ArrayList<Integer>(lottoNumber.subList(0, 6));
+        //오름차순 정렬
+        Collections.sort(lottoNumber);
+
+        //로또 번호를 출력해주는 for문
+        printLottoNumbers(lottoNumber);
+
+        return lottoNumber;
     }
 
     //로또 출력 메서드
-    static void lottoNumber(int[] lottoName) {
-        //중복 제거하여 배열에 값 저장 for문
-        for (int i = 0; i < 6; i++) {
-            lottoName[i] = (int) (Math.random() * 45) + 1;
-            // 중복 번호 제거
-            for (int j = 0; j < i; j++) {
-                if (lottoName[i] == lottoName[j]) {
-                    i--;
-                    break;
-                }
-            }
-        }
-        //배열 오름차순 정렬
-        Arrays.sort(lottoName);
-
-        //로또 번호를 출력해주는 for문
-        for(int i = 0; i < 6; i++){
-            System.out.printf("%2d", lottoName[i]);
-            if(i < 5){
-                System.out.print(", ");
-            }
+    public static void printLottoNumbers(ArrayList<Integer> lottoNumber){
+        for(int number : lottoNumber){
+            System.out.printf("%2d ", number);
         }
     }
 }
